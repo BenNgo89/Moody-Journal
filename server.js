365c37ffd0df9939a145678d58e396e3c09f38e0
salmonshoe <rafaelreynoso273@gmail.com>
2019-05-20 19:28:00 -0400

    sequelize changes along with passport and handlebars file removal

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
+var bodyParser = require('body-parser');
+let session = require('express-session');
+var passport = require("./config/passport");
 
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({ extended: false })); //For body parser
+app.use(bodyParser.json());
+  //Use Sessions to keep track of user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
 
 var syncOptions = { force: false };
 
