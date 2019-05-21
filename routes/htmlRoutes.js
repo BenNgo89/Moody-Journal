031c2ea9da61f8e1c4a59f7d0641e48df58cdc14
salmonshoe <rafaelreynoso273@gmail.com>
2019-05-20 21:12:02 -0400

    Sequelize integration with OAuth for our login page. Basic HTML skeleton added for testing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,4 @@
-var db = require("../models");
+/*var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
@@ -25,5 +25,31 @@ module.exports = function(app) {
     res.render("404");
   });
 };
+*/
 
-//Test Login Page
+//Path helps direct routes to HTML files and OAuth middleware to check for user log ins
+var path = require("path");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    //Users are redirected to /members if they already have an account
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    //Users are redirected to /members if they already have an account
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  //Authentication added to /members so users who are not logged in get redirected to sign up
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+};
