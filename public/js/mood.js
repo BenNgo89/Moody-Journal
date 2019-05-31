7946d055a9cbcd2c7590b55f5e42874388952c71
salmonshoe <rafaelreynoso273@gmail.com>
2019-05-30 21:58:20 -0400

    added mood.js front end and a readme file

---
+++ public/js/mood.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  //var $newJournalEntry = $(#user-type); to reference input field of what user journals
+
+  //Here are event listeners for button/link functionalities
+  $(document).on("click", "#fineMood", insertFine);
+  $(document).on("click", "#happyMood", insertHappy);
+  $(document).on("click", "#badMood", insertBad);
+  $(document).on("click", "#terribleMood", insertTerrible);
+
+  function insertFine() {
+    var feeling = {
+      mood: "fine",
+      value: 2
+    };
+    $.post("/api/diary", feeling);
+  }
+
+  function insertHappy() {
+    var feeling = {
+      mood: "happy",
+      value: 3
+    };
+
+    $.post("/api/diary", feeling);
+  }
+
+  function insertBad() {
+    var feeling = {
+      mood: "bad",
+      value: 1
+    };
+
+    $.post("/api/diary", feeling);
+  }
+
+  function insertTerrible() {
+    var feeling = {
+      mood: "terrible",
+      value: 0
+    };
+
+    $.post("/api/diary", feeling);
+  }
+});
